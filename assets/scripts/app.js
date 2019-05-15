2eea6b17923d614a0045357f74accc4e9fbd1cf9
ericbowman <ericjbowman@gmail.com>
2019-05-15 15:53:03 -0400

    Write win scenarios

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,11 @@
-// 'use strict'
-// const events = require(./events.js)
+'use strict'
+const events = require('./events.js')
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-// $(() => {
-//   $('.box').on('click', events.fillContent)
-// })
+$(() => {
+  $('.box').on('click', events.fillContent)
+})
--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,9 +1,54 @@
-// 'use strict'
-//
-// const fillContent = function() {
-//   $(event.target).html("o")
+'use strict'
+// const zero = $('.zero')
+const moveArr = []
+const checkForWin = function () {
+  if ($('.zero').html() === $('one').html() && $('one').html() === $('.two').html()) {
+    alert('you win!')
+  }
+}
+const fillContent = function() {
+  if (moveArr.length === 9) {
+    return
+  } else if (moveArr.length % 2 !== 0) {
+    $(event.target).html("O")
+    moveArr.push('O')
+  } else if (moveArr.length % 2 === 0) {
+    $(event.target).html('X')
+    moveArr.push('X')
+  }
+  checkForWin()
+  const lines = {
+    rowOne: [$('.zero').html(), $('.one').html(), $('.two').html()],
+    rowTwo: [$('.three').html(), $('.four').html(), $('.five').html()],
+    rowThree: [$('.six').html(), $('.seven').html(), $('.eight').html()],
+    columnOne: [$('.zero').html(), $('.three').html(), $('.six').html()],
+    columnTwo: [$('.one').html(), $('.four').html(), $('.five').html()],
+    columnThree: [$('.two').html(), $('.five').html(), $('.six').html()]
+  }
+  console.log(lines)
+}
+
+// const eventChain = {
+//   fillContent: function() {
+//     if (moveArr.length === 9) {
+//       return
+//     } else if (moveArr.length % 2 !== 0) {
+//       $(event.target).html("O")
+//       moveArr.push('O')
+//     } else if (moveArr.length % 2 === 0) {
+//       $(event.target).html('X')
+//       moveArr.push('X')
+//       console.log(moveArr)
+//     }
+//   },
+//   checkForWin: function () {
+//     if (moveArr[0] === movArr[1] && moveArr[1] === moveArr[3]) {
+//       alert('you win!')
+//     }
+//   }
 // }
-//
-// module.exports = {
-//   fillContent
-// }
+
+module.exports = {
+  fillContent,
+  checkForWin
+}
